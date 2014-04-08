94ace987102f77bbe923415fdb28c27b61f72a68
David Pershouse <dpershouse@gmail.com>
2014-04-08 16:59:25 +1000

    make go faster?

--- CainRR/OwinWebApi/Startup.cs
+++ CainRR/OwinWebApi/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Microsoft.Owin.Host.HttpListener;
 using Owin;
 
 namespace OwinWebApi
@@ -14,6 +15,13 @@ namespace OwinWebApi
         // parameter in the WebApp.Start method.
         public void Configuration(IAppBuilder appBuilder)
         {
+
+            var owinHttpListener = (OwinHttpListener)appBuilder.Properties["Microsoft.Owin.Host.HttpListener.OwinHttpListener"];
+            //Default for maxAccepts = 5 * ProcCount
+            //Default for maxRequests = Int32.MaxValue;
+            owinHttpListener.SetRequestProcessingLimits(20, Int32.MaxValue);
+
+
             // Configure Web API for self-host. 
             HttpConfiguration config = new HttpConfiguration();
             config.Routes.MapHttpRoute(
